809d507ee5f3fe42450ed421cfde05c242909c62
HungPham0307 <hungpham0307@gmail.com>
2017-11-29 09:13:25 +0700

    + add function activeImg to acitve change images

--- js/surfers-co.js
+++ js/surfers-co.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   function activeTab(obj) {
     //xoa class active
     $('.content-text-slide li a').removeClass('active');
@@ -23,5 +24,31 @@ $(document).ready(function() {
     activeTab(this);
     return false;
   });
+
+
+  function activeImg(obj) {
+    //xoa class active
+    $('.list-image-slide li img').removeClass('active-images');
+    //add class active vao img hien tai
+    $(obj).addClass('active-images');
+
+    //lay id de hien anh dc click
+    var id = $(obj).attr('alt');
+
+    //an het anh dang hien thi
+    $('.content-image-slide').hide();
+
+    //hien thi anh dc active
+    $(id).css('display', 'inline-block');
+  }
+
+  activeImg($('.list-image-slide li:first-child img'));
+
+  $('.list-image-slide li img').click(function(){
+    activeImg(this);
+    return false;
+  });
+
+
 
 })
